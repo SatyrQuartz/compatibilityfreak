14ce3b4c9b88549162b7caee06c03fced11e4c7e
Colby Ray <colbydray@gmail.com>
2021-01-15 09:39:22 -0500

    Add files via upload

---
+++ installers/uninstall_houdini.sh
@@ -0,0 +1,8 @@
+rm /usr/libexec/houdini64
+rm /usr/libexec/houdini
+rmdir /usr/libexec
+rm /usr/lib/houdini/cpu64
+rm /usr/lib/houdini/cpu32
+rm /usr/lib/houdini/ld.so  
+rm /usr/lib/houdini/ld64.so
+rm -rf /usr/lib/houdini
