79807c24a3c80777cb9475623ca5586ceb57b5b4
colbydray <colbydray@gmail.com>
2022-05-15 20:32:42 -0400

    Update houdini32.sh

--- houdini32.sh
+++ houdini32.sh
@@ -1,1 +1,5 @@
-LD_LIBRARY_PATH=/emuroot/arm-linux-gnueabihf/usr/lib/arm-linux-gnueabihf/:/emuroot/arm-linux-gnueabihf/usr/lib/arm-linux-gnueabihf/pulseaudio/ /usr/libexec/houdini $@
+#!/bin/bash
+all_args=("$@")
+first_arg=$1
+rest_args=("${all_args[@]:1}")
+LD_LIBRARY_PATH=/emuroot/arm-linux-gnueabihf/usr/lib/arm-linux-gnueabihf/:/emuroot/arm-linux-gnueabihf/usr/lib/arm-linux-gnueabihf/pulseaudio/ /usr/libexec/houdini $all_args . $rest_args
